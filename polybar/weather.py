5c76940aaf9f246eb70655ce039a00bf526062ee
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-08-26 15:46:51 -0400

    Adds polybar config.

---
+++ polybar/weather.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+import urllib.request, json
+
+city = "30297"
+api_key = "ba345b9219e4508665da076f3599e816"
+units = "Imperial"
+unit_key = "F"
+
+weather = eval(
+    str(
+        urllib.request.urlopen(
+            "http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".
+            format(city, api_key, units)).read())[2:-1])
+
+info = weather["weather"][0]["description"].capitalize()
+temp = int(float(weather["main"]["temp"]))
+
+# print("%s, %i°%s" % (info, temp, unit_key))
+print("%i°%s" % (temp, unit_key))
