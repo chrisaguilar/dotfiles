5aee13f6700d22ebf69f6df7550996ff1f54088f
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-03 16:01:08 -0400

    Adds more extensions.

--- _install/vscode.sh
+++ _install/vscode.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-pacaur -Syu visual-studio-code
+pacaur -Syu --needed visual-studio-code
 
 # from `code --list-extensions`
 extensions="
+    christian-kohler.path-intellisense
+    dbaeumer.vscode-eslint
     donjayamanne.python
+    EditorConfig.EditorConfig
+    eg2.tslint
+    eg2.vscode-npm-script
+    esbenp.prettier-vscode
+    formulahendry.code-runner
+    HookyQR.beautify
+    humao.rest-client
+    joelday.docthis
+    msjsdiag.debugger-for-chrome
+    octref.vetur
     robertohuertasm.vscode-icons
+    shinnn.stylelint
     zhuangtongfa.Material-theme
 "
 for extension in $extensions; do
-    code --install-extension "$extension" || true
+    code --install-extension "$extension"
 done
