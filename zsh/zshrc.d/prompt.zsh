f04a01c06a1a7df87900cee1e9a353323881bb23
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-16 08:44:13 -0400

    Fixes incorrect prompt formatting.

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -3,7 +3,7 @@ autoload -Uz vcs_info
 # Enable git Repositories
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' formats ' (%F{yellow}%r%f@%F{green}%b%f%c%u) '
-zstyle ':vcs_info:*' actionformats '(%F{yellow}%r%f@%F{green}%b%f%c%u:$F{red}%a%f)'
+zstyle ':vcs_info:*' actionformats '(%F{yellow}%r%f@%F{green}%b%f%c%u:%F{red}%a%f)'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr '|%F{blue}S%f'
 zstyle ':vcs_info:*' unstagedstr '|%F{magenta}U%f'
