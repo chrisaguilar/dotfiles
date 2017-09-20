43d12b560bb1e7dd5e5644c114cd4d0746e90de0
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-20 07:43:12 -0400

    Now I know what's happening when it fails.

--- _archlinux/install.sh
+++ _archlinux/install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+Bold=$(tput bold)
+Reset=$(tput sgr0)
+Green=$(tput setaf 2)
+BGreen=${Bold}${Green}
+
 arch_chroot() {
     arch-chroot /mnt /bin/bash -c "${1}"
 }
@@ -13,58 +18,58 @@ read -p "Root Partition (e.g. sda2): " BOOT_MOUNTPOINT
 
 pacman -Sy
 
-# Keymap
+echo "${BGreen}Set the Keymap${Reset}"
 loadkeys us
 
-# Sync the Clock
+echo "${BGreen}Sync the Clock${Reset}"
 timedatectl set-ntp true
 
-# Mirrorlist
+echo "${BGreen}Get the Fastest Mirrors${Reset}"
 pacman -Sy --noconfirm reflector
 reflector --save /etc/pacman.d/mirrorlist --verbose --sort rate -f 10 -a 6 -p https -c US
 
-# Install Base System
+echo "${BGreen}Install the Base System${Reset}"
 pacstrap /mnt base base-devel git stow vim zsh reflector
 
-# Generate Fstab
+echo "${BGreen}Generate Fstab${Reset}"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Create Swap File
+echo "${BGreen}Create Swap File${Reset}"
 fallocate -l 8G /mnt/var/swapfile
 chmod 600 /mnt/var/swapfile
 mkswap /mnt/var/swapfile
 swapon /mnt/var/swapfile
 echo -e "/var/swapfile\tnone\tswap\tdefaults\t0\t0" >> /mnt/etc/fstab
 
-# Timezone
+echo "${BGreen}Set the Timezone${Reset}"
 arch_chroot "ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime"
 arch_chroot "sed -i '/#NTP=/d' /etc/systemd/timesyncd.conf"
 arch_chroot "sed -i 's/#Fallback//' /etc/systemd/timesyncd.conf"
 arch_chroot "echo \"FallbackNTP=0.pool.ntp.org 1.pool.ntp.org 0.fr.pool.ntp.org\" >> /etc/systemd/timesyncd.conf"
 
-# Hardware Clock
+echo "${BGreen}Sync the Hardware Clock${Reset}"
 arch_chroot "hwclock --systohc --utc"
 
-# Locale
+echo "${BGreen}Set the Locale${Reset}"
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /mnt/etc/locale.gen
 arch_chroot "locale-gen"
 
-# Keymap
-echo "KEYMAP=en_US.UTF-8" > /mnt/etc/vconsole.conf
+echo "${BGreen}Set the Keymap${Reset}"
+echo "KEYMAP=us" > /mnt/etc/vconsole.conf
 
-# Hostname
+echo "${BGreen}Set the Hostname${Reset}"
 echo "chris" >> /mnt/etc/hostname
 sed -i.bak -r -e "s/# End of file/127.0.1.1\tchris.localdomain\tchris/" /mnt/etc/hosts
 
-# Network
+echo "${BGreen}Enable Networking${Reset}"
 arch_chroot "pacman -Sy --noconfirm networkmanager dnsmasq openresolv dhclient nm-connection-editor gnome-keyring"
 arch_chroot "systemctl enable NetworkManager"
 
-# Initramfs
+echo "${BGreen}Initramfs${Reset}"
 arch_chroot "mkinitcpio -p linux"
 
-# Install & Configure Bootloader
+echo "${BGreen}Install & Configure Bootloader${Reset}"
 root_partuuid=$(basename `find /dev/disk/by-partuuid/ -type l -ilname "*${BOOT_MOUNTPOINT}*"`)
 
 arch_chroot "bootctl install"
@@ -81,9 +86,9 @@ initrd   /initramfs-linux.img
 options  root=PARTUUID=$root_partuuid rw quiet loglevel=3 udev.log-priority=3
 EOF
 
-# Root Password
+echo "${BGreen}Set the Root Password${Reset}"
 arch_chroot "passwd"
 
-# Finish
+echo "${BGreen}Finish${Reset}"
 umount -R /mnt
 reboot
