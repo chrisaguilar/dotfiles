41237fd44e28bd257f8dc6d3a38a7f5e9ef09453
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-21 19:53:58 -0400

    Moves LS_COLOS to $ZDOTDIR/zshrc.d

--- zsh/zshrc.d/00-first.zsh
+++ zsh/zshrc.d/00-first.zsh
@@ -1,11 +1,11 @@
 # Cache directory setup
 ZSH_CACHE="/tmp/.zsh-${USER}-${ZSH_VERSION}"
-mkdir -p $ZSH_CACHE
-chmod 700 $ZSH_CACHE
+mkdir -p "${ZSH_CACHE}"
+chmod 700 "${ZSH_CACHE}"
 
 # Misc
 
 ttyctl -f
 
 # LS_COLORS
-eval $( dircolors -b $ZDOTDIR/LS_COLORS )
+eval $( dircolors -b "${ZDOTDIR}/zshrc.d/LS_COLORS" )
