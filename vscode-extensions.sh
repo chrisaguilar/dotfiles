d05594e57b7194a26723edfc865d747e35a73d74
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-28 19:25:51 -0400

    Adds extension.

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 extensions="
-EditorConfig.EditorConfig
-donjayamanne.githistory
-donjayamanne.python
-eg2.tslint
-esbenp.prettier-vscode
-joelday.docthis
-robertohuertasm.vscode-icons
-zhuangtongfa.Material-theme
+    EditorConfig.EditorConfig
+    James-Yu.latex-workshop
+    donjayamanne.githistory
+    donjayamanne.python
+    eg2.tslint
+    esbenp.prettier-vscode
+    joelday.docthis
+    robertohuertasm.vscode-icons
+    zhuangtongfa.Material-theme
 "
 
 for extension in ${extensions}; do
