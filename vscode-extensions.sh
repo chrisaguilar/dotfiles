d2fc719ab61f48fb368a6d3fe0c9ff1efcfc73ae
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-12-11 18:04:03 -0500

    Updates extensions.

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
 
 extensions="
-    EditorConfig.EditorConfig
-    James-Yu.latex-workshop
+    abusaidm.html-snippets
+    CoenraadS.bracket-pair-colorizer
+    dbaeumer.vscode-eslint
     donjayamanne.githistory
-    donjayamanne.python
+    eamodio.gitlens
+    EditorConfig.EditorConfig
     eg2.tslint
     esbenp.prettier-vscode
+    formulahendry.auto-close-tag
+    formulahendry.auto-rename-tag
+    formulahendry.code-runner
+    HookyQR.beautify
+    humao.rest-client
     joelday.docthis
+    ms-python.python
+    msjsdiag.debugger-for-chrome
+    PeterJausovec.vscode-docker
+    redhat.java
     robertohuertasm.vscode-icons
+    robinbentley.sass-indented
+    shinnn.stylelint
+    vscjava.vscode-java-debug
+    vscjava.vscode-java-pack
+    wayou.vscode-todo-highlight
     zhuangtongfa.Material-theme
+    Zignd.html-css-class-completion
 "
 
 for extension in ${extensions}; do
