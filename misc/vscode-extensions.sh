b6b2d2f7638935845de914f4d10711c27335e9f1
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-12-15 01:16:39 -0500

    Did a thing

--- misc/vscode-extensions.sh
+++ misc/vscode-extensions.sh
@@ -1,34 +1,5 @@
 #!/usr/bin/env bash
 
-extensions="
-    abusaidm.html-snippets
-    CoenraadS.bracket-pair-colorizer
-    dbaeumer.vscode-eslint
-    donjayamanne.githistory
-    eamodio.gitlens
-    EditorConfig.EditorConfig
-    eg2.tslint
-    esbenp.prettier-vscode
-    formulahendry.auto-close-tag
-    formulahendry.auto-rename-tag
-    formulahendry.code-runner
-    HookyQR.beautify
-    humao.rest-client
-    joelday.docthis
-    ms-python.python
-    msjsdiag.debugger-for-chrome
-    PeterJausovec.vscode-docker
-    redhat.java
-    robertohuertasm.vscode-icons
-    robinbentley.sass-indented
-    shinnn.stylelint
-    vscjava.vscode-java-debug
-    vscjava.vscode-java-pack
-    wayou.vscode-todo-highlight
-    zhuangtongfa.Material-theme
-    Zignd.html-css-class-completion
-"
-
-for extension in ${extensions}; do
+for extension in `cat ./extensions`; do
     code --install-extension "${extension}"
 done
