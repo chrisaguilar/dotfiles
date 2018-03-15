0b92ba8bfba568f8c3c70cb42204ff86d4eb9bbd
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-03-15 10:31:36 -0400

    Fix grammar in _archlinux/postinstall/95-misc.sh
    For a smoother experience.

--- _archlinux/postinstall/95-misc.sh
+++ _archlinux/postinstall/95-misc.sh
@@ -1,7 +1,7 @@
 title "Miscellaneous"
 
 
-subtitle "Silence fsck Messages"
+subtitle "Silencing fsck Messages"
 sed -i -r -e 's/HOOKS=.*$/HOOKS="base udev autodetect modconf block filesystems keyboard"/' /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
@@ -11,7 +11,7 @@ echo -e "StandardOutput=null\nStandardError=journal+console" >> /etc/systemd/sys
 echo -e "StandardOutput=null\nStandardError=journal+console" >> /etc/systemd/system/systemd-fsck-root.service
 
 
-subtitle "Setup Automatic Login to Virtual Console"
+subtitle "Turning On Automatic Login to Virtual Console"
 mkdir -p /etc/systemd/system/getty@tty1.service.d
 cat << "EOF" > /etc/systemd/system/getty@tty1.service.d/override.conf
 [Service]
@@ -20,41 +20,41 @@ ExecStart=-/usr/bin/agetty --autologin chris --noclear %I $TERM
 EOF
 
 
-subtitle "Blacklist Modules"
+subtitle "Blacklisting Modules"
 echo -e "blacklist nouveau" > /etc/modprobe.d/blacklist.conf
 
 
-subtitle "Update the mlocate Database"
+subtitle "Updating the mlocate Database"
 updatedb
 
 
-subtitle "Detect Sensors"
+subtitle "Detecting Sensors"
 sensors-detect --auto >> "${LOG}" 2>&1
 
 
-subtitle "Enable Network Time Synchronization"
+subtitle "Enabling Network Time Synchronization"
 timedatectl set-ntp true
 
 
-subtitle "Increase fs.inotify.max_user_watches"
+subtitle "Increasing fs.inotify.max_user_watches"
 echo "fs.inotify.max_user_watches=524288" > /etc/sysctl.d/40-max-user-watches.conf
 
 
-subtitle "Disable Watchdog"
+subtitle "Disabling Watchdog"
 echo "kernel.nmi_watchdog=0" > /etc/sysctl.d/40-disable-watchdog.conf
 
 
-subtitle "Modify journald Usage"
+subtitle "Modifying journald Usage"
 sed -i "s/#SystemMaxUse.*/SystemMaxUse=100M/" /etc/systemd/journald.conf
 
 
-subtitle "Modify coredump Usage"
+subtitle "Modifying coredump Usage"
 sed -i 's/#MaxUse.*/MaxUse=100M/' /etc/systemd/coredump.conf
 
 
-subtitle "Modify /etc/dhcpcd.conf"
+subtitle "Modifying /etc/dhcpcd.conf"
 echo "noarp" >> /etc/dhcpcd.conf
 sed -i '/option ntp_servers/s/^#//' /etc/dhcpcd.conf
 
-subtitle "Enable System Services"
+subtitle "Enabling System Services"
 enable_services "pkgstats.timer fstrim.timer avahi-daemon.service sshd.service"
