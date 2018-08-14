edf007b61f1588002cff662e2923ed7d3ad6cf9a
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-08-14 15:38:42 -0400

    fix: Disable Heroku autocompletion

--- zsh/zshrc.d/heroku.zsh
+++ zsh/zshrc.d/heroku.zsh
@@ -1,5 +1,5 @@
 # type heroku &>/dev/null && heroku autocomplete:script zsh
 
-HEROKU_AC_ZSH_SETUP_PATH=/home/chris/.cache/heroku/autocomplete/zsh_setup
+# HEROKU_AC_ZSH_SETUP_PATH=/home/chris/.cache/heroku/autocomplete/zsh_setup
 
-test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH
+# test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH
